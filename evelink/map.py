fb8c30d3c81618fe6efee805dc88876e16f89fb0
Amber Yust <amber.yust@gmail.com>
2012-07-18 19:48:27 +0000

    Add map/Kills wrapper

--- evelink/map.py
+++ evelink/map.py
@@ -27,3 +27,32 @@ class Map(object):
         data_time = api.parse_ts(api_result.find('dataTime').text)
 
         return results, data_time
+
+    def kills_by_system(self):
+        """Get kill counts for systems in the last hour.
+
+        Returns a tuple of ({system:{killdata}, timestamp).
+
+        Each {killdata} is {'faction':count, 'ship':count, 'pod':count}.
+        """
+
+        api_result = self.api.get('map/Kills')
+
+        rowset = api_result.find('rowset')
+        results = {}
+        for row in rowset.findall('row'):
+            system = int(row.attrib['solarSystemID'])
+            faction_kills = int(row.attrib['factionKills'])
+            ship_kills = int(row.attrib['shipKills'])
+            pod_kills = int(row.attrib['podKills'])
+
+            results[system] = {
+                'id': system,
+                'faction': faction_kills,
+                'ship': ship_kills,
+                'pod': pod_kills,
+            }
+
+        data_time = api.parse_ts(api_result.find('dataTime').text)
+
+        return results, data_time
--- tests/test_map.py
+++ tests/test_map.py
@@ -25,3 +25,29 @@ class MapTestCase(APITestCase):
         self.assertEqual(self.api.mock_calls, [
                 mock.call.get('map/Jumps'),
             ])
+
+    def test_kills_by_system(self):
+        self.api.get.return_value = self.make_api_result(r"""
+            <result>
+                <rowset name="solarSystems">
+                    <row solarSystemID="30001343" shipKills="0" factionKills="17" podKills="0" />
+                    <row solarSystemID="30002671" shipKills="1" factionKills="34" podKills="0" />
+                    <row solarSystemID="30005327" shipKills="5" factionKills="21" podKills="1" />
+                </rowset>
+                <dataTime>2007-12-16 10:57:53</dataTime>
+            </result>
+        """)
+
+        result = self.map.kills_by_system()
+
+        self.assertEqual(result, (
+                {
+                    30001343: {'id':30001343, 'faction':17, 'ship':0, 'pod':0},
+                    30002671: {'id':30002671, 'faction':34, 'ship':1, 'pod':0},
+                    30005327: {'id':30005327, 'faction':21, 'ship':5, 'pod':1},
+                },
+                1197802673,
+            ))
+        self.assertEqual(self.api.mock_calls, [
+                mock.call.get('map/Kills'),
+            ])
