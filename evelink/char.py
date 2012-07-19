ca214f0b38490ec60892284f15bf4618b3860977
Dan Boger <dan@peeron.com>
2012-07-18 23:00:05 -0700

    Add kill_log API

--- evelink/char.py
+++ evelink/char.py
@@ -17,11 +17,11 @@ class Char(object):
 
         rowset = api_result.find('rowset')
         row = rowset.find('row')
-        result = {
-                'balance': float(row.attrib['balance']),
-                'id': int(row.attrib['accountID']),
-                'key': int(row.attrib['accountKey']),
-            }
+        result = { 
+            'balance': float(row.attrib['balance']),
+            'id': int(row.attrib['accountID']),
+            'key': int(row.attrib['accountKey']),
+        }
         return result
 
     def wallet_balance(self, character_id):
@@ -86,4 +86,92 @@ class Char(object):
                 'pause_ts': api.parse_ts(a['pauseProductionTime']),
             }
 
+        return result
+
+    def kills(self, character_id, before_kill=None):
+        """Look up recent kills for the given character
+
+        character_id: 
+            The ID of the character to look up kills for.
+        before_kill:
+            Optional. Only show kills before this kill id. (Used for paging.)
+        """
+
+        params = {'characterID': character_id}
+        if before_kill is not None:
+            params['beforeKillID'] = before_kill
+        api_result = self.api.get('char/KillLog', params)
+
+        rowset = api_result.find('rowset')
+        result = {}
+        for row in rowset.findall('row'):
+            a = row.attrib
+            kill_id = int(a['killID'])
+            result[kill_id] = {
+                'id': kill_id,
+                'system_id': int(a['solarSystemID']),
+                'time': api.parse_ts(a['killTime']),
+                'moon_id': int(a['moonID']),
+            }
+
+            victim = row.find('victim')
+            a = victim.attrib
+            result[kill_id]['victim'] = {
+                'id': int(a['characterID']),
+                'name': a['characterName'],
+                'corp': {
+                    'id': int(a['corporationID']),
+                    'name': a['corporationName'],
+                },
+                'alliance': {
+                    'id': int(a['allianceID']),
+                    'name': a['allianceName'],
+                },
+                'faction': {
+                    'id': int(a['factionID']),
+                    'name': a['factionName'],
+                },
+                'damage': int(a['damageTaken']),
+                'ship_type_id': int(a['shipTypeID']),
+            }
+
+            result[kill_id]['attackers'] = {}
+            attackers = row.find("rowset[@name='attackers']")
+            for attacker in attackers.findall('row'):
+                a = attacker.attrib
+                attacker_id = int(a['characterID'])
+                result[kill_id]['attackers'][attacker_id] = {
+                    'id': attacker_id,
+                    'name': a['characterName'],
+                    'corp': {
+                        'id': int(a['corporationID']),
+                        'name': a['corporationName'],
+                    },
+                    'alliance': {
+                        'id': int(a['allianceID']),
+                        'name': a['allianceName'],
+                    },
+                    'faction': {
+                        'id': int(a['factionID']),
+                        'name': a['factionName'],
+                    },
+                    'sec_status': float(a['securityStatus']),
+                    'damage': int(a['damageDone']),
+                    'final_blow': a['finalBlow'] == '1',
+                    'weapon_type_id': int(a['weaponTypeID']),
+                    'ship_type_id': int(a['shipTypeID']),
+                }
+
+            result[kill_id]['items'] = {}
+            items = row.find("rowset[@name='items']")
+            for item in items.findall('row'):
+                a = item.attrib
+                type_id = int(a['typeID'])
+                result[kill_id]['items'][type_id] = {
+                    'id': type_id,
+                    'flag': int(a['flag']),
+                    'dropped': int(a['qtyDropped']),
+                    'destroyed': int(a['qtyDestroyed']),
+                }
+            
         return result
