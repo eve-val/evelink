13a34660ffbf9701491ef3dccdf2a42aaf3e6517
Amber Yust <amber.yust@gmail.com>
2012-07-22 23:55:26 +0000

    Add account/APIKeyInfo

--- evelink/account.py
+++ evelink/account.py
@@ -1,4 +1,5 @@
 from evelink import api
+from evelink import constants
 
 class Account(object):
     """Wrapper around /account/ of the EVE API.
@@ -21,3 +22,30 @@ class Account(object):
             'logins': _int('logonCount'),
             'minutes_played': _int('logonMinutes'),
         }
+
+    def key_info(self):
+        """Returns the details of the API key being used to auth."""
+
+        api_result = self.api.get('account/APIKeyInfo')
+
+        key = api_result.find('key')
+        result = {
+            'access_mask': int(key.attrib['accessMask']),
+            'type': constants.APIKey.key_types[key.attrib['type']],
+            'expire_ts': api.parse_ts(key.attrib['expires']) if key.attrib['expires'] else None,
+            'characters': {},
+        }
+
+        rowset = key.find('rowset')
+        for row in rowset.findall('row'):
+            character = {
+                'id': int(row.attrib['characterID']),
+                'name': row.attrib['characterName'],
+                'corp': {
+                    'id': int(row.attrib['corporationID']),
+                    'name': row.attrib['corporationName'],
+                },
+            }
+            result['characters'][character['id']] = character
+
+        return result
--- tests/test_account.py
+++ tests/test_account.py
@@ -2,6 +2,7 @@ import mock
 import unittest2 as unittest
 
 import evelink.account as evelink_account
+from evelink import constants
 from tests.utils import APITestCase
 
 class AccountTestCase(APITestCase):
@@ -23,6 +24,30 @@ class AccountTestCase(APITestCase):
             })
         self.assertEqual(self.api.mock_calls, [
                 mock.call.get('account/AccountStatus'),
+            ])
+
+    def test_key_info(self):
+        self.api.get.return_value = self.make_api_result("account/key_info.xml")
+
+        result = self.account.key_info()
+
+        self.assertEqual(result, {
+                'access_mask': 59760264,
+                'type': constants.CHARACTER,
+                'expire_ts': 1315699200,
+                'characters': {
+                    898901870: {
+                        'id': 898901870,
+                        'name': "Desmont McCallock",
+                        'corp': {
+                            'id': 1000009,
+                            'name': "Caldari Provisions",
+                        },
+                    },
+                },
+            })
+        self.assertEqual(self.api.mock_calls, [
+                mock.call.get('account/APIKeyInfo'),
             ])
 
 
