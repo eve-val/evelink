da85e11965f09bb7f490931aa6c83290b63b0979
Amber Yust <amber.yust@gmail.com>
2012-08-18 01:37:46 +0000

    Make a real __init__.py for evelink

---
+++ evelink/__init__.py
@@ -0,0 +1,23 @@
+"""EVELink - Python bindings for the EVE API."""
+
+from evelink import account
+from evelink import api
+from evelink import char
+from evelink import constants
+from evelink import corp
+from evelink import eve
+from evelink import map
+from evelink import parsing
+from evelink import server
+
+__all__ = [
+  "account",
+  "api",
+  "char",
+  "constants",
+  "corp",
+  "eve",
+  "map",
+  "parsing",
+  "server",
+]
