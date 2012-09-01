17e27f869af29aec881044f1325e8a978346c936
Silas Snider <swsnider@gmail.com>
2012-08-31 20:44:52 -0700

    remove the memcache tests due to appengine testbed bug and simplicity of implementation

--- tests/test_appengine.py
+++ tests/test_appengine.py
@@ -1,36 +1,9 @@
 import unittest2 as unittest
 from google.appengine.ext import testbed
 
-#from StringIO import StringIO
-
-#import mock
+import mock
 
 from evelink import appengine
-
-
-class MemcacheCacheTestCase(unittest.TestCase):
-    def setUp(self):
-        self.testbed = testbed.Testbed()
-        self.testbed.activate()
-        self.testbed.init_memcache_stub()
-
-    def tearDown(self):
-        self.testbed.deactivate()
-
-    def test_cache_memcache(self):
-        cache = appengine.AppengineCache()
-        cache.put('foo', 'bar', 3600)
-        cache.put('bar', 1, 3600)
-        cache.put('baz', True, 3600)
-        self.assertEqual(cache.get('foo'), 'bar')
-        self.assertEqual(cache.get('bar'), 1)
-        self.assertEqual(cache.get('baz'), True)
-
-    def test_expire_memcache(self):
-        cache = appengine.AppengineCache()
-        cache.put('baz', 'qux', -1)
-        self.assertEqual(cache.get('baz'), None)
-
 
 class DatastoreCacheTestCase(unittest.TestCase):
     def setUp(self):
