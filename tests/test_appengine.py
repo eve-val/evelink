84b331ef84c02825da5e0cd9e0f355e9cc331f21
Silas Snider <swsnider@gmail.com>
2012-09-12 20:08:02 -0700

    added appengine dependency for tests

--- tests/test_appengine.py
+++ tests/test_appengine.py
@@ -1,8 +1,6 @@
 import unittest2 as unittest
 from google.appengine.ext import testbed
 
-import mock
-
 from evelink import appengine
 
 class DatastoreCacheTestCase(unittest.TestCase):
@@ -25,6 +23,30 @@ class DatastoreCacheTestCase(unittest.TestCase):
 
     def test_expire_datastore(self):
         cache = appengine.AppengineDatastoreCache()
+        cache.put('baz', 'qux', -1)
+        self.assertEqual(cache.get('baz'), None)
+
+
+class MemcacheCacheTestCase(unittest.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_memcache_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_cache_memcache(self):
+        cache = appengine.AppengineCache()
+        cache.put('foo', 'bar', 3600)
+        cache.put('bar', 1, 3600)
+        cache.put('baz', True, 3600)
+        self.assertEqual(cache.get('foo'), 'bar')
+        self.assertEqual(cache.get('bar'), 1)
+        self.assertEqual(cache.get('baz'), True)
+
+    def test_expire_memcache(self):
+        cache = appengine.AppengineCache()
         cache.put('baz', 'qux', -1)
         self.assertEqual(cache.get('baz'), None)
 
