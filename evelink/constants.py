5ef53e031109b126bf82222be1b720c6480141b8
Amber Yust <amber.yust@gmail.com>
2013-12-28 19:45:46 -0800

    Add missing 'aborted' to job status constants

--- evelink/constants.py
+++ evelink/constants.py
@@ -29,7 +29,7 @@ class Corp(object):
         )
 
 class Industry(object):
-    job_status = ('failed', 'delivered', 'gm-aborted', 'inflight-unanchored', 'destroyed')
+    job_status = ('failed', 'delivered', 'aborted', 'gm-aborted', 'inflight-unanchored', 'destroyed')
 
 class Market(object):
     order_status = ('active', 'closed', 'expired', 'cancelled', 'pending', 'deleted')
