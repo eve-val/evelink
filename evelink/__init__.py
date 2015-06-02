9ab1530a010e974a376c75da806016185199c545
Amber Yust <amber.yust@gmail.com>
2015-06-01 21:01:36 -0700

    Update version to 0.7.0 for release

--- evelink/__init__.py
+++ evelink/__init__.py
@@ -11,7 +11,7 @@ from evelink import eve
 from evelink import map
 from evelink import server
 
-__version__ = "0.6.2"
+__version__ = "0.7.0"
 
 # Implement NullHandler because it was only added in Python 2.7+.
 class NullHandler(logging.Handler):
